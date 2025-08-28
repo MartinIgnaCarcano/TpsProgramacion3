23972609bae715645f86fa896415b39ee0c15055
MartinPc <martin.ignacio.carcano@gmail.com>
2025-08-28 19:35:45 -0300

    actividad2

---
+++ 5. toString(), etc/actividad2/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.entidades.Estudiante;
+
+public class Main {
+    public static void main(String[] args) {
+        Estudiante estu = new Estudiante("Martin",24,"ingenieria");
+        System.out.println(estu);
+    }
+}
