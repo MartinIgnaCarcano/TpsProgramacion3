0faf24889f089017708489655b47d1ebe08c5767
MartinPc <martin.ignacio.carcano@gmail.com>
2025-09-04 13:03:38 -0300

    7. Lombok y DTO(actividad2)

--- 7. Lombok y DTO/Actividades7/src/main/java/org/example/Main.java
+++ 7. Lombok y DTO/Actividades7/src/main/java/org/example/Main.java
@@ -6,5 +6,18 @@ public class Main {
         System.out.println(new Persona("Fer",27));
         System.out.println(new Persona("Seba",15));
         System.out.println(new Persona("Rodri",17));
+
+        Usuario usuario = Usuario.builder()
+                .id(123)
+                .nombre("Martin")
+                .email("martin@prueba.com")
+                .build();
+        Usuario usuario1 = Usuario.builder()
+                .id(132)
+                .nombre("Paco")
+                .email("paco@prueba.com")
+                .build();
+        System.out.println(usuario);
+        System.out.println(usuario1);
     }
 }
---
+++ 7. Lombok y DTO/Actividades7/src/main/java/org/example/Usuario.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String email;
+}
