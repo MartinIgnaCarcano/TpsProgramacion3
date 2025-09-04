4c2906cbb0c11ccf6cff60a6a5d35175bcba2f27
MartinPc <martin.ignacio.carcano@gmail.com>
2025-09-04 12:55:59 -0300

    7.Lombok y DTO(activadad1)

---
+++ 7. Lombok y DTO/Actividades7/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Persona());
+        System.out.println(new Persona("Martin",24));
+        System.out.println(new Persona("Fer",27));
+        System.out.println(new Persona("Seba",15));
+        System.out.println(new Persona("Rodri",17));
+    }
+}
---
+++ 7. Lombok y DTO/Actividades7/src/main/java/org/example/Persona.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Persona {
+    private String nombre;
+    private int edad;
+}
