e54e207b73cc7a82c7347820ae6e394dd1abe27e
MartinPc <martin.ignacio.carcano@gmail.com>
2025-09-04 12:42:39 -0300

    Actividad 3-4 (Colecciones)

--- 6. Colecciones/Actividades/src/main/java/org/example/Main.java
+++ 6. Colecciones/Actividades/src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("------------------------------------");
+        System.out.println("Actividad 2");
+        System.out.println("------------------------------------");
         List<Alumno> alumnos = new ArrayList<>();
         alumnos.add(new Alumno("Juan", 8.5));
         alumnos.add(new Alumno("María", 9.2));
@@ -21,6 +23,36 @@ public class Main {
         for (Alumno alumno : alumnos) {
             System.out.println(alumno);
         }
+
+        System.out.println("------------------------------------");
+        System.out.println("Actividad 3");
+        System.out.println("------------------------------------");
+        Set<Producto>productos = new HashSet<>();
+        productos.add(new Producto(12345678,"descripcion1"));
+        productos.add(new Producto(12345679,"descripcion2"));
+        productos.add(new Producto(12345610,"descripcion3"));
+        productos.add(new Producto(12345678,"descripcion1"));//repetido
+        productos.add(new Producto(12345679,"descripcion2"));//repetido
+        productos.add(new Producto(12345611,"descripcion4"));
+
+        for (Producto producto: productos){
+            System.out.println(producto);
+        }
+        System.out.println("------------------------------------");
+        System.out.println("Actividad 4");
+        System.out.println("------------------------------------");
+        Map<String, Curso> cursos = new HashMap<>();
+        cursos.put("P3",new Curso("Prog3","Alvaro Gonzales"));
+        cursos.put("P2",new Curso("Prog2","Alvaro Perez"));
+        cursos.put("P1",new Curso("Prog1","Alvaro Garcia"));
+        cursos.put("P0",new Curso("Prog0","Alvaro Martinez"));
+
+        System.out.println(cursos.get("P3"));
+        System.out.println(cursos.get("P0"));
+        for (Map.Entry<String, Curso> entry : cursos.entrySet()) { //
+            System.out.println("Clave: " + entry.getKey() + ", Valor: " + entry.getValue()); //
+        }
+
     }
 
     }
---
+++ 6. Colecciones/Actividades/src/main/java/org/example/Producto.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Producto {
+    private int codigo;
+    private String descripcion;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return codigo == producto.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo=" + codigo +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
